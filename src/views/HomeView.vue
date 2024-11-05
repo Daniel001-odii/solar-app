81b375931b9154f72ef30cd89c25e9be51c9c111
Odii Daniel <xenithheight@gmail.com>
2024-11-05 09:35:14 +0100

    update to app ui and functionality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,16 +21,26 @@
      <div v-if="screen == 1" class="page-screen">
       <!-- GROUP 1 -->
        <div class="self-start p-5 mt-5">
-        <h1 class="font-bold text-3xl">MCE Solar Inverter</h1>
-        <p>126 KW/H - Discharging</p>
+        <h1 class="font-bold text-3xl">MCE Smart Solar Inverter</h1>
+        <div class="flex flex-row gap-3">
+           <p>126 KW/H - Discharging</p>
+        </div>
+
+       <!--  <div class="my-4 w-full flex justify-center items-center border">
+          <span class="loader"></span>
+        </div>
+        -->
        </div>
 
        <!-- GROUP 2 -->
-       <p class="mt-6 text-2xl opacity-50">Device Control</p>
+       <p class="mt-6 text-2xl opacity-50">Device Control - 
+        <span v-if="is_on" class=" text-green-500">ON</span>
+        <span v-else class=" text-red-500">OFF</span>
+      </p>
 
        <!-- GROUP 3 -->
        <div class="p-4 mt-6">
-        <button class=" size-[200px] rounded-full border bg-black text-white hover:scale-95 outline-offset-4 outline-black outline flex justify-center items-center">
+        <button @click="togglePower()" :class="is_on ? 'bg-green-500 outline-green-500 shadow-2xl shadow-green-400':'bg-black outline-black'" class=" size-[200px] rounded-full border  text-white hover:scale-95 outline-offset-4 outline flex justify-center items-center">
           <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
             <path d="M7.5 1v7h1V1z"/>
             <path d="M3 8.812a5 5 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812"/>
@@ -71,7 +81,7 @@
       <div v-if="screen == 2" class="page-screen">
       <!-- GROUP 1 -->
        <div class="self-start p-5 mt-5">
-        <h1 class="font-bold text-3xl">MCE Solar Inverter</h1>
+        <h1 class="font-bold text-3xl">MCE Smart Solar Inverter</h1>
         <p>126 KW/H - Discharging</p>
        </div>
 
@@ -148,10 +158,31 @@
     data() {
       return {
         screen: 0,
+        // 192, 168, 4, 22
+        is_on: false,
       }
     },
+
+    methods:{
+      togglePower(){
+        this.is_on = !this.is_on;
+        console.log('button clicked');
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://192.168.4.22/toggle', true);
+        xhr.send();
+      }
+    }
   }
 </script>
+
+<!-- <script> 
+  function toggleOutput(){ 
+    console.log('button clicked'); 
+    let xhr = new XMLHttpRequest(); 
+    xhr.open('GET', '/toggle', true); 
+    xhr.send(); 
+  }
+</script> -->
 
 <style scoped>
 
@@ -185,7 +216,7 @@ progress[value]::-webkit-progress-value {
   }
 
   .page-screen{
-    @apply bg-white flex items-center w-[400px] h-screen lg:max-h-[900px] relative flex-col
+    @apply bg-green-100 flex items-center w-[400px] h-screen lg:max-h-[900px] relative flex-col
   }
 
   .fade-enter-active, .fade-leave-active {
@@ -198,5 +229,67 @@ progress[value]::-webkit-progress-value {
   .stat-box{
     @apply border rounded-3xl border-black p-5 flex flex-col min-w-[150px]
   }
-  
+
+
+
+.glowing::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-radius: inherit;
+  background: radial-gradient(circle, #23AA1B, transparent);
+  filter: blur(10px);
+  opacity: 0.6;
+  animation: glow 1.5s infinite alternate ease-in-out;
+  z-index: -1;
+}
+
+@keyframes glow {
+  from {
+    transform: scale(1);
+    opacity: 0.6;
+  }
+  to {
+    transform: scale(1.3);
+    opacity: 1;
+  }
+}
+
+
+.loader {
+  width: 48px;
+  height: 48px;
+  border: 5px solid #02c268;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  position: relative;
+  animation: pulse 1s linear infinite;
+}
+.loader:after {
+  content: '';
+  position: absolute;
+  width: 48px;
+  height: 48px;
+  border: 5px solid #0ca74f;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  animation: scaleUp 1s linear infinite;
+}
+
+@keyframes scaleUp {
+  0% { transform: translate(-50%, -50%) scale(0) }
+  60% , 100% { transform: translate(-50%, -50%)  scale(1)}
+}
+@keyframes pulse {
+  0% , 60% , 100%{ transform:  scale(1) }
+  80% { transform:  scale(1.2)}
+}
 </style>
